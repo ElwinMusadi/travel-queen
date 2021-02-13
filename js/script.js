51e63b0891529c1dcefb631b52d7008ea3840e35
ElwinMusadi <elwinmusadi@gmail.com>
2021-02-13 10:10:11 +0800

    Menambahkan smooth scrolling dengan jQuery

--- js/script.js
+++ js/script.js
@@ -1,20 +1,6 @@
 const text = document.querySelector('.hero-text').innerHTML;
 const navbar = document.querySelector('.navbar');
 
-function scaleText(string) {
-  const strArr = string.split('');
-  let span = '';
-  // return strArr.map(s => span += `<span>${s}</span>`);
-  for( let i = 0; i < strArr.length; i++ ) {
-    if( strArr[i] != ' ' ) {
-      span += `<span>${strArr[i]}</span>`;
-    } else {
-      span += ' ';
-    }
-  }
-  
-  return document.querySelector('.hero-text').innerHTML = span;
-}
 
 window.onscroll = () => {
   if( this.scrollY >= 10 ) {
@@ -24,5 +10,34 @@ window.onscroll = () => {
   }
 }
 
-// console.log(navbarColored(navbar));
-scaleText(text);
+$('.nav-link').on('click', function(e) {
+  
+  const tujuan = $(this).attr('href');
+  const elemenTujuan = $(tujuan);
+  
+  const jarak = 100;
+  $('html').animate({
+    scrollTop: elemenTujuan.offset().top - jarak
+  }, 800);
+
+  e.preventDefault();
+});
+
+
+
+
+
+// function scaleText(string) {
+//   const strArr = string.split("");
+//   let span = "";
+//   for (let i = 0; i < strArr.length; i++) {
+//     if (strArr[i] != " ") {
+//       span += `<span>${strArr[i]}</span>`;
+//     } else {
+//       span += " ";
+//     }
+//   }
+
+//   return (document.querySelector(".hero-text").innerHTML = span);
+// }
+// scaleText(text);
